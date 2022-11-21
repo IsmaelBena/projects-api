4a3c4f5e3c0788a34d650a73ee721dd84686f609
IsmaelBena <Ismael.Benadjal@gmail.com>
2022-11-21 14:38:03 +0000

    attempt to fix cors problem

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ var corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 
 const projectsRouter = require('./routes/projects');
